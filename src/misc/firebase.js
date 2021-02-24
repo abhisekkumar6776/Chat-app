050b19e58fb75d0dc517bd07d42655a07cf00ac1
ab-srivastav <abhisek5898@gmail.com>
2021-02-24 18:26:47 +0530

    added sign in page and sign in with facebook and google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -3,17 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  
-    apiKey: "AIzaSyASGA-rI2JrX7CCT_BIkQONND7RgMt4JHY",
-    authDomain: "chat-web-app-d0bb1.firebaseapp.com",
-    databaseURL: "https://chat-web-app-d0bb1-default-rtdb.firebaseio.com",
-    projectId: "chat-web-app-d0bb1",
-    storageBucket: "chat-web-app-d0bb1.appspot.com",
-    messagingSenderId: "603528922817",
-    appId: "1:603528922817:web:b663e22229bdb40a557640"
-  
+  apiKey: 'AIzaSyASGA-rI2JrX7CCT_BIkQONND7RgMt4JHY',
+  authDomain: 'chat-web-app-d0bb1.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-d0bb1-default-rtdb.firebaseio.com/',
+  projectId: 'chat-web-app-d0bb1',
+  storageBucket: 'chat-web-app-d0bb1.appspot.com',
+  messagingSenderId: '603528922817',
+  appId: '1:603528922817:web:b663e22229bdb40a557640',
 };
-
 
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
